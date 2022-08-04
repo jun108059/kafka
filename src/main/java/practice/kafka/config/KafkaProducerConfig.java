17531bfead2ed5e9648229ead84782cc4b9c60c4
YoungJun Park <youngjun108059@gmail.com>
2022-08-04 23:49:32 +0900

    🔧 Producer 설정 파일 추가

---
+++ src/main/java/practice/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,41 @@
+package practice.kafka.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * KafkaTemplate - Topic 으로 데이터 보내는 편의 기능 설정(By Producer)
+ */
+@EnableKafka
+@Configuration
+@RequiredArgsConstructor
+public class KafkaProducerConfig {
+
+    private final KafkaConstants kafkaConstants;
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConstants.getProducerBootStrapServers());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+}
